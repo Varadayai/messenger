f0b436575f0751adbb7f150b241d752ba052c059
saurabh <sindhu.saurabh7@gmail.com>
2015-02-04 15:06:42 +0530

    Initial Commit

---
+++ messenger/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  messenger
+//
+//  Created by saurabh sindhu on 04/02/15.
+//  Copyright (c) 2015 saurabh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ messenger/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  messenger
+//
+//  Created by saurabh sindhu on 04/02/15.
+//  Copyright (c) 2015 saurabh. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
